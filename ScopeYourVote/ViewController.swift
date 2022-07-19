643b9030b1221ef7609131cc9ffafbdd58d25bd9
Mariam Ndao <ndaomariam25@gmail.com>
2022-07-19 11:51:49 -0700

    Initial Commit

---
+++ ScopeYourVote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScopeYourVote
+//
+//  Created by Mariam Ndao on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
